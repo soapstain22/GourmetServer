051dc39ac2201f37d731755c905e7f8df8314e1e
superpizza <36314018+soapstain22@users.noreply.github.com>
2023-08-09 12:12:48 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
-java -jar BuildTools.jar --rev 1.20.1
-git pull
-java -Xmx8g -jar spigot-1.20.1.jar -nogui 
+while true
+do
+    java -jar BuildTools.jar --rev 1.20.1
+    git pull
+    java -Xmx8g -jar spigot-1.20.1.jar -nogui 
+done
